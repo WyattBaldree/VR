fa4d12bd42bb1d6ff48c489a9b17322a58018612
WyattBaldree <wyattmbaldree@gmail.com>
2020-11-16 13:48:43 -0500

    Added ragdoll prefab. Imported the synty prototype assets. Moved stuff around.

---
+++ Assets/MyAssets/Scripts/Dude.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Dude : MonoBehaviour
+{
+    [SerializeField] private GameObject ragdoll;
+    [SerializeField] private GameObject animatedModel;
+    [SerializeField] private NavMeshAgent navmeshAgent;
+
+    private bool dead;
+
+    private void Awake()
+    {
+        ragdoll.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ToggleDead();
+        }
+    }
+
+    [ContextMenu("ToggleDead")]
+    private void ToggleDead()
+    {
+        dead = !dead;
+        if (dead)
+        {
+            //switch to the ragdoll.
+            CopyTransformData(animatedModel.transform, ragdoll.transform, navmeshAgent.velocity);
+            ragdoll.gameObject.SetActive(true);
+            animatedModel.gameObject.SetActive(false);
+            navmeshAgent.enabled = false;
+        }
+        else
+        {
+            //switch back to the model and disable the ragdoll.
+            ragdoll.gameObject.SetActive(false);
+            animatedModel.gameObject.SetActive(true);
+            navmeshAgent.enabled = true;
+        }
+    }
+
+    private void CopyTransformData(Transform sourceTransform, Transform destinationTransform, Vector3 velocity)
+    {
+        if(sourceTransform.childCount != destinationTransform.childCount)
+        {
+            Debug.LogWarning("CopyTranformData requires that both transforms have the same hierarchy.");
+            return;
+        }
+
+        for (int i = 0; i < sourceTransform.childCount; i++)
+        {
+            var source = sourceTransform.GetChild(i);
+            var destination = destinationTransform.GetChild(i);
+            destination.position = source.position;
+            destination.rotation = source.rotation;
+
+            var rb = destination.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.velocity = velocity;
+            }
+
+            CopyTransformData(source, destination, velocity);
+        }
+    }
+}
---
+++ Assets/MyAssets/Scripts/RandomRoam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class RandomRoam : MonoBehaviour
+{
+    private NavMeshAgent navmeshAgent;
+
+    private void Awake()
+    {
+        navmeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(navmeshAgent.enabled == false)
+        {
+            return;
+        }
+        if (navmeshAgent.hasPath == false || navmeshAgent.remainingDistance < 1f)
+        {
+            ChooseNewPosition();
+        }
+    }
+
+    private void ChooseNewPosition()
+    {
+        Vector3 randomOffset = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+        var destination = transform.position + randomOffset;
+        navmeshAgent.SetDestination(destination);
+    }
+}
